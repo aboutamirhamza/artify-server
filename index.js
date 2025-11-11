6735f3ec4abb3a1283388abdc5c089541529611a
aboutamirhamza <amirhamzashuvokhan@gmail.com>
2025-11-11 21:05:11 +0600

    Server Install express cors mongo install also mongo connect and art post complete

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 8000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://artifyDB:vzy2dGw6Pr1Vgn0W@cluster0.otzdp54.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Connecting to Artify Server");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db("artify_db");
+    const artWorkCollection = db.collection("artworks");
+
+    app.post("/artworks", async (req, res) => {
+      const newArtwork = req.body;
+      const result = await artWorkCollection.insertOne(newArtwork);
+      res.status(201).send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    //
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Artify Server listening at ${port}`);
+});
