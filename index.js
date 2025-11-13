be75a98aca36d30a789c0dcfd35b620eea024ca5
aboutamirhamza <amirhamzashuvokhan@gmail.com>
2025-11-13 16:41:45 +0600

    new Changes

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
     const db = client.db("artify_db");
     const artWorkCollection = db.collection("artworks");
     const userCollection = db.collection("users");
